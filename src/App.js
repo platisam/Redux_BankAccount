6059568d1eeb3fb81aad03bc647a8ce99cdf061e
platisam <platisam@gmail.com>
2023-07-22 19:40:54 +0200

    added BalanceDisplay component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import CreateCustomer from "./CreateCustomer";
 import AccountOperations from "./AccountOperations";
 import Customer from "./Customer";
+import BalanceDisplay from "./BalanceDisplay";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <CreateCustomer />
       <Customer />
       <AccountOperations />
+      <BalanceDisplay />
     </div>
   );
 }
