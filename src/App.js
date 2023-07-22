3ce92f8ca8cff01fd31164e38eecf0780998a7b9
platisam <platisam@gmail.com>
2023-07-22 19:38:12 +0200

    added Customer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import CreateCustomer from "./CreateCustomer";
 import AccountOperations from "./AccountOperations";
+import Customer from "./Customer";
 
 function App() {
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
       <CreateCustomer />
+      <Customer />
       <AccountOperations />
     </div>
   );
