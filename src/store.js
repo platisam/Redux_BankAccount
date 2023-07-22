29ee6f1f553be7030715f23bb96e33510a9bd91d
platisam <platisam@gmail.com>
2023-07-22 22:24:20 +0200

    adding store.js file for redux in isolation and creating reducer function

---
+++ src/store.js
@@ -0,0 +1,28 @@
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  // the goal of the reducer is to calculate the new state based on the current state and received action
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+}
