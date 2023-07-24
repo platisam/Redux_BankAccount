4a18e82fb84c3f89119519899a08ad88ba8813bf
platisam <platisam@gmail.com>
2023-07-24 11:18:19 +0200

    conditional display of components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import CreateCustomer from "./features/customers/CreateCustomer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import Customer from "./features/customers/Customer";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((store) => store.customer.fullName);
+
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {fullName === "" ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
