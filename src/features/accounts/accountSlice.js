2bbc515ef42ea162abbea8b372631657a4e29372
platisam <platisam@gmail.com>
2023-07-25 17:12:40 +0200

    creating the accountSlice using redux toolkit (requestLoan and payLoan)

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -19,6 +19,20 @@ const accountSlice = createSlice({
       //state.balance = state.balance - action.payload;
       state.balance -= action.payload;
     },
+    requestLoan(state, action) {
+      if (state.loan === 0) return;
+
+      state.loan = action.payload.amount;
+      state.loanPurpose = action.payload.purpose;
+      state.balance = state.balance + action.payload.amount;
+      //state.balance += action.payload.amount;
+    },
+    payLoan(state, action) {
+      state.loan = 0;
+      state.loanPurpose = "";
+      //state.balance = state.balance - state.loan;
+      state.balance -= state.loan;
+    },
   },
 });
 
