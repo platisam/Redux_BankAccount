a8a6a12984cfe1559a2ed0f6aa557cf2e6453046
platisam <platisam@gmail.com>
2023-07-25 18:42:30 +0200

    setting thunk for converted currency

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -14,6 +14,7 @@ const accountSlice = createSlice({
     deposit(state, action) {
       //state.balance = state.balance + action.payload;
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       //state.balance = state.balance - action.payload;
@@ -35,16 +36,34 @@ const accountSlice = createSlice({
         //state.balance += action.payload.amount;
       },
     },
-    payLoan(state, action) {
+    payLoan(state) {
       state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = "";
       //state.balance = state.balance - state.loan;
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    // return action
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
 export default accountSlice.reducer;
 
